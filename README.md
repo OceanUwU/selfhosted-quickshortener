cccc91c44b150bf60e2eacc503557552fd3d42ad
Ocean <s@ocean.lol>
2021-05-20 20:08:17 +0100

    a

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
         "key": "v3uSzCuahqA7v22i" //https://www.random.org/strings/?num=16&len=16&digits=on&upperalpha=on&loweralpha=on&unique=on&format=html&rnd=new
     }
     ```
-    and edit them to your needs.
+    and edit them to your needs (also remove the comments).
 6. `npx sequelize-cli db:migrate`
 7. `npm start`
 
@@ -24,4 +24,9 @@
 (will do once extension is reviewed)
 
 ### Firefox
-(will do once firefox extension is done)
+1. [Download the add-on](https://addons.mozilla.org/firefox/downloads/file/3779538/quickshortener-1.0.2-fx.xpi)
+2. Right click the extension icon in the top right of firefox
+3. Click "Manage Extension"
+4. Choose the "Options" tab
+5. Fill in the options and click save
+6. Go to the page of any link you want to shorten and click the extension icon to shorten it
--- chrome/popup.js
+++ chrome/popup.js
@@ -11,8 +11,20 @@ function generate() {
             } else {
                 $('#link').val(data);
                 $('#link')[0].select();
-                document.execCommand('copy');
-                $('#success').text('Copied to clipboard!');
+                if (document.execCommand('copy')) {
+                    $('#success').text('Copied to clipboard!');
+                } else {
+                    let button = $('<button class="btn btn btn-primary">Copy</button>');
+                    button.click(() => {
+                        $('#link')[0].select();
+                        if (document.execCommand('copy')) {
+                            $('#success').text('Copied to clipboard!');
+                        } else {
+                            $('#success').text('Could not copy.');
+                        }
+                    });
+                    $('#success').append(button);
+                }
             }
         }).fail(() => alert('Could not connect to host specified in options.'));
     });
@@ -23,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
         $('#host').text(options.host);
     });
 
-    chrome.tabs.getSelected(null, tab => {
+    chrome.tabs.getSelected(null, tab => { //firefox: browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT}).then(tabs => browser.tabs.get(tabs[0].id)).then(tab => {
         $('#source').val('generateRandomLink');
         $('#destination').val(tab.url);
         generate();
